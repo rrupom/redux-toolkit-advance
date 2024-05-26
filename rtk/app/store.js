2033d49985653ae069dd7c6a52b0a231d8ef54c9
rrupom <rakib.t.rupom@gmail.com>
2024-05-26 08:33:09 +0600

    middleware in rtk

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,12 +1,18 @@
 const { configureStore } = require("@reduxjs/toolkit")
 const counterReducer = require("../features/counter/counterSlice")
 const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice")
+const { createLogger } = require("redux-logger")
+
+const logger = createLogger();
 
 // configure store
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         dynamicCounter: dynamicCounterReducer
+    },
+    middleware: (getDefaultMiddlewares) => {
+        return getDefaultMiddlewares().concat(logger);
     }
 })
 
